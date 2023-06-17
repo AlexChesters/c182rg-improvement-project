1fde06757502f1d07d202ccfc32e8623fc5e8dad
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2023-06-17 19:56:12 +0100

    save fuel

--- c182rg-improvement-project/html_ui/Pages/VCockpit/Instruments/ShermheadRyder/SRC182RG.js
+++ c182rg-improvement-project/html_ui/Pages/VCockpit/Instruments/ShermheadRyder/SRC182RG.js
@@ -1,24 +1,68 @@
-'use strict';
+'use strict'
 
 class SRC182RG extends BaseInstrument {
   constructor() {
-    super();
+    super()
     console.log('src182rg')
+
+    var titleFromSimvar = SimVar.GetSimVarValue('TITLE', 'string')
+    this.aircraftIdentifier = titleFromSimvar.replace(/\s+/g, '_')
+    this.shouldTrackPersistence = false
   }
+
+  //load the gauge template - found in SRC182RG.HTML
+  get templateID() { return 'SRC182RG' }
 
   // runs every frame
   Update() {
     super.Update()
+    this.persistState()
   }
 
-  onFlightStart() {
-    console.log('onFlightStart');
-    super.onFlightStart();
-    SimVar.SetSimVarValue("FUEL TANK LEFT MAIN QUANTITY", "gallons", Number(80));
+  persistState() {
+    if (!this.shouldTrackPersistence) return
+
+    console.log('persisting state')
+
+    try {
+      var leftTankVolume = SimVar.GetSimVarValue('FUEL TANK LEFT MAIN QUANTITY', 'gallons')
+      var rightTankVolume = SimVar.GetSimVarValue('FUEL TANK RIGHT MAIN QUANTITY', 'gallons')
+      
+      console.log('left volume', leftTankVolume)
+      console.log('right volume', rightTankVolume)
+      
+      SetStoredData('SRC182RG_LEFT_FUEL_TANK_'+ this.aircraftIdentifier, leftTankVolume.toString())
+      SetStoredData('SRC182RG_RIGHT_FUEL_TANK_'+ this.aircraftIdentifier, rightTankVolume.toString())
+    } catch (ex) {
+      console.error('error persisting state', ex)
+    }
   }
 
-  //load the gauge template - found in SRC182RG.HTML
-  get templateID() { return 'SRC182RG'; }
+  applyState() {
+    var leftTankStoredVolume = GetStoredData('SRC182RG_LEFT_FUEL_TANK_'+ this.aircraftIdentifier)
+    var rightTankStoredVolume = GetStoredData('SRC182RG_RIGHT_FUEL_TANK_'+ this.aircraftIdentifier)
+
+    console.log('applying left volume', leftTankStoredVolume)
+    console.log('applying right volume', rightTankStoredVolume)
+    
+    SimVar.SetSimVarValue('FUEL TANK LEFT MAIN QUANTITY', 'gallons', Number(leftTankStoredVolume || 10))
+    SimVar.SetSimVarValue('FUEL TANK RIGHT MAIN QUANTITY', 'gallons', Number(rightTankStoredVolume || 10))
+  }
+  
+  // 0 = main menu
+  // 1 = loading
+  // 2 = briefing
+  // 3 = in game
+  onGameStateChanged(oldState, newState) {
+    super.onGameStateChanged()
+    
+    console.log('onGameStateChanged', oldState, newState)
+    if (!oldState && newState === 3) {
+      this.applyState()
+      this.shouldTrackPersistence = true
+    }
+  }
+
 }
 
 registerInstrument('src182rg-element', SRC182RG)
