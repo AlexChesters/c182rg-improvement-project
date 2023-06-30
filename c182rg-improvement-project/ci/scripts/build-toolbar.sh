5304e81b11f7a84085b800dcf723fab82ff09ca8
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2023-06-30 16:37:37 +0100

    fix icon

--- c182rg-improvement-project/ci/scripts/build-toolbar.sh
+++ c182rg-improvement-project/ci/scripts/build-toolbar.sh
@@ -2,6 +2,6 @@ set -e
 
 ./fspackagetool.exe ./c182rg-improvement-project-ingamepanels.xml -nopause
 cp -R html_ui/InGamePanels dist/html_ui/InGamePanels
-cp -R html_ui/Textures dist/html_ui/Textures
+cp -R html_ui/icons dist/html_ui/icons
 cp -R InGamePanels dist/InGamePanels
 cp -R Packages/c182rg-improvement-project-ingamepanels/Build/c182rg-improvement-project-ingamepanels.spb dist/InGamePanels/c182rg-improvement-project-ingamepanels.spb
